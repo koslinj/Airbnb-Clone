53db5f760b2637243643ee6d3a0ba4d593f2e9f5
koslinj <koslinj@gmail.com>
2024-03-29 16:48:11 +0100

    listings map component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,11 +4,17 @@ import { Link, Stack } from 'expo-router'
 import ExploreHeader from '@/components/ExploreHeader'
 import Listings from '@/components/Listings'
 import listingsData from "@/assets/data/airbnb-listings.json"
+import listingsDataGeo from "@/assets/data/airbnb-listings.geo.json"
+import ListingsMap from '@/components/ListingsMap'
 
 const Page = () => {
   const [category, setCategory] = useState("Tiny homes")
   const items = useMemo(() => {
-    return (listingsData as any[]).filter(item => item.medium_url !== null)
+    return (listingsData as any[]).slice(0,700).filter(item => item.medium_url !== null)
+  }, [])
+  const itemsGeo = useMemo(() => {
+    const json = listingsDataGeo as any
+    return (json.features as any[]).slice(0,700).filter(item => item.medium_url !== null)
   }, [])
 
   const onDataChanged = (category: string) => {
@@ -22,7 +28,8 @@ const Page = () => {
           header: () => <ExploreHeader onCategoryChanged={onDataChanged} />
         }}
       />
-      <Listings listings={items} category={category} />
+      {/* <Listings listings={items} category={category} /> */}
+      <ListingsMap listings={itemsGeo} />
     </View>
   )
 }
---
+++ components/ListingsMap.tsx
@@ -0,0 +1,90 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React, { useEffect } from 'react'
+import { Marker } from 'react-native-maps'
+import { defaultStyles } from '@/constants/Styles'
+import * as Location from 'expo-location';
+import { useRouter } from 'expo-router';
+import Colors from '@/constants/Colors';
+import MapView from 'react-native-map-clustering';
+
+interface Props {
+  listings: any
+}
+
+const INITIAL_REGION = {
+  latitude: 45.438759,
+  longitude: 12.327145,
+  latitudeDelta: 0.9,
+  longitudeDelta: 0.9
+}
+
+const ListingsMap = ({ listings }: Props) => {
+  const router = useRouter()
+
+  const onMarkerSelected = (item: any) => {
+    router.push(`/listing/${item.properties.id}`)
+  }
+
+  useEffect(() => {
+    (async () => {
+
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        //setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      //let location = await Location.getCurrentPositionAsync({});
+      //setLocation(location);
+    })();
+  }, []);
+
+  return (
+    <View style={defaultStyles.container}>
+      <MapView
+        animationEnabled={false}
+        style={StyleSheet.absoluteFill}
+        provider='google'
+        showsUserLocation
+        showsMyLocationButton
+        initialRegion={INITIAL_REGION}
+        clusterColor='#fff'
+        clusterTextColor='#000'
+        clusterFontFamily='mon-sb'
+      >
+        {listings.map((item: any) => (
+          <Marker
+            onPress={() => onMarkerSelected(item)}
+            key={item.properties.id}
+            coordinate={{
+              latitude: +item.properties.latitude,
+              longitude: +item.properties.longitude,
+            }}
+          >
+            <View style={styles.marker}>
+              <Text style={styles.markerText}>€{item.properties.price}</Text>
+            </View>
+          </Marker>
+        ))}
+      </MapView>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  marker: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+    padding: 3,
+    borderColor: '#000',
+    borderWidth: 1,
+    borderRadius: 12
+  },
+  markerText: {
+    fontSize: 12,
+    fontFamily: 'mon-sb'
+  }
+});
+
+export default ListingsMap
