bcdf3ac7009c7c11a71bc7a8fc6e2cbf0ff89eb6
koslinj <koslinj@gmail.com>
2024-03-29 18:19:18 +0100

    add custom locate button, which finally allows to turn up location after launching app

--- components/ListingsMap.tsx
+++ components/ListingsMap.tsx
@@ -1,52 +1,64 @@
 import { View, Text, StyleSheet } from 'react-native'
-import React, { memo, useEffect } from 'react'
+import React, { memo, useEffect, useRef, useState } from 'react'
 import { Marker } from 'react-native-maps'
 import { defaultStyles } from '@/constants/Styles'
 import * as Location from 'expo-location';
 import { useRouter } from 'expo-router';
 import Colors from '@/constants/Colors';
 import MapView from 'react-native-map-clustering';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { Ionicons } from '@expo/vector-icons';
 
 interface Props {
   listings: any
 }
 
-const INITIAL_REGION = {
-  latitude: 45.438759,
-  longitude: 12.327145,
-  latitudeDelta: 0.9,
-  longitudeDelta: 0.9
-}
-
 const ListingsMap = memo(({ listings }: Props) => {
   const router = useRouter()
+  const mapRef = useRef<MapView>(null)
 
   const onMarkerSelected = (item: any) => {
     router.push(`/listing/${item.properties.id}`)
   }
 
+  const onLocationBtn = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      //setErrorMsg('Permission to access location was denied');
+      return;
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    (mapRef as any).current?.animateToRegion({
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+      longitudeDelta: 0.004,
+      latitudeDelta: 0,
+    });
+  }
+
   useEffect(() => {
     (async () => {
-
       let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        //setErrorMsg('Permission to access location was denied');
-        return;
-      }
-
-      //let location = await Location.getCurrentPositionAsync({});
-      //setLocation(location);
     })();
   }, []);
+
+  const INITIAL_REGION = {
+    latitude: 45.438759,
+    longitude: 12.327145,
+    latitudeDelta: 0.9,
+    longitudeDelta: 0.9
+  }
 
   return (
     <View style={defaultStyles.container}>
       <MapView
+        ref={mapRef}
         animationEnabled={false}
         style={StyleSheet.absoluteFill}
         provider='google'
         showsUserLocation
-        showsMyLocationButton
+        showsMyLocationButton={false}
         initialRegion={INITIAL_REGION}
         clusterColor='#fff'
         clusterTextColor='#000'
@@ -67,11 +79,29 @@ const ListingsMap = memo(({ listings }: Props) => {
           </Marker>
         ))}
       </MapView>
+      <View style={styles.viewAbsolute}>
+        <TouchableOpacity style={styles.locateButton} onPress={onLocationBtn}>
+          <Ionicons name='location' size={28} />
+        </TouchableOpacity>
+      </View>
     </View>
   )
 })
 
 const styles = StyleSheet.create({
+  viewAbsolute: {
+    position: 'absolute',
+    right: 20,
+    bottom: 82,
+  },
+  locateButton: {
+    padding: 8,
+    borderRadius: 50,
+    backgroundColor: "#fff",
+    elevation: 2,
+    borderColor: Colors.grey,
+    borderWidth: 1
+  },
   marker: {
     alignItems: 'center',
     justifyContent: 'center',
